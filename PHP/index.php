75f2c9dff4ccd3503109c3e43b499f9ca9ade296
Gabriel-Yukio <gabrielyukiomibecoca@gmail.com>
2022-06-22 19:24:39 -0300

    menu V1 pronto

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,125 @@
     <link rel="stylesheet" href="../CSS/style.css">
     <title>Projeto 1</title>
 </head>
+
 <body>
-    
+    <header class="topo">
+
+        <div class="logo">
+            <h2>RaMiGaLeVi</h2>
+        </div>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Categorias</a></li>
+            <li><a href="#">Pedidos</a></li>
+        </ul>
+        <ul class="user">
+            <li>
+                <a href="#">
+                    <ion-icon name="cart"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <ion-icon name="person"></ion-icon>
+                </a>
+            </li>
+
+        </ul>
+    </header>
+    <main class="principal">
+        <!-- -------------------------- Produto -------------------------- -->
+
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+
+        <!-- -------------------------- Produto -------------------------- -->
+
+        <!-- -------------------------- Produto teste -------------------------- -->
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+        <div class="produto">
+            <div class="produto_img">
+                <img src="../IMG/pimenta_teste.png" alt="">
+            </div>
+            <div class="produto_desc">
+                <h3>molho de pimenta </h3>
+                <h5>R$: 20,00</h5>
+                <input type="submit" class="btn_compra" value="Comprar">
+            </div>
+        </div>
+
+    </main>
+
+
+    <script type="module" class="btn_compra" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
